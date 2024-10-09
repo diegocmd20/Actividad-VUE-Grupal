d79524efa1ef4e8fb7e6bcfd3d54cb5731cf631e
Diego <diegocmd20@gmail.com>
2024-10-09 19:33:56 -0300

    Activity 1 and 2 done

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import Actividad1 from './components/Actividad1.vue' 
+  import Actividad2 from './components/Actividad2.vue'
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <Actividad2/>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
---
+++ src/components/Actividad1.vue
@@ -0,0 +1,21 @@
+<!-- 1.-  Input de texto simple
+Enunciado: Crea un componente que tenga un campo de texto donde el usuario pueda escribir su nombre. El nombre debe mostrarse de forma dinámica en un h1 usando v-model. -->
+
+<template>
+    <input v-model="user" type="text" placeholder="Ingresa tu nombre"/>
+    <h1>{{ user }}</h1>
+</template>
+
+<script>
+export default {
+    name: 'Actividad1',
+    data() {
+        return {
+            user:''
+        };
+    }
+};
+</script>
+
+<style scoped>
+</style>
