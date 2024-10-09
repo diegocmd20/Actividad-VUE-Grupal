2caa06f6d2bf6d8eaf1a3e102af181ac291a1548
josé Pérez <j.igna.perez@gmail.com>
2024-10-09 19:59:32 -0300

    descripcion del boton añadida

--- src/components/Actividad8.vue
+++ src/components/Actividad8.vue
@@ -2,7 +2,7 @@
 Enunciado: Crea un componente que tenga un botón. El botón debe tener una clase CSS dinámica que cambie según un valor booleano en el data. Usa v-bind:class para aplicar la clase condicionalmente. -->
 <template>
   <div>
-    <button @click="selector=!selector" :class="selector ? clase1:clase2"></button>
+    <button @click="selector=!selector" :class="selector ? clase1:clase2">Haz click para cambiar de color</button>
   </div>
 </template>
 
