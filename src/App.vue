8481afe4c1194ed30c07473f4fc91a7cf26290ed
josé Pérez <j.igna.perez@gmail.com>
2024-10-09 20:05:17 -0300

    app.vue template actualizado

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 import Actividad3 from './components/Actividad3.vue'
 import Actividad4 from './components/Actividad4.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
   <Actividad3/>
   <Actividad4/>
 </template>
